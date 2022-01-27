9e5cb038e0da34315f1dfcf3bfdf963cff169d84
linux_china <libing.chen@gmail.com>
2022-01-26 18:49:06 -0800

    HttpxApplication

---
+++ src/main/java/org/mvnsearch/http/HttpxApplication.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpxApplication {
+
+    public static void main(String[] args) {
+        System.exit(SpringApplication.exit(SpringApplication.run(HttpxApplication.class, args)));
+    }
+
+}
