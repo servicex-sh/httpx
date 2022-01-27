4ac58b61b30652fa399244d55326561efc70201d
linux_china <libing.chen@gmail.com>
2022-01-26 18:51:59 -0800

    RSocketExecutorTest

---
+++ src/test/java/org/mvnsearch/http/protocol/RSocketExecutorTest.java
@@ -0,0 +1,44 @@
+package org.mvnsearch.http.protocol;
+
+
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.http.model.HttpRequest;
+import org.mvnsearch.http.model.HttpRequestParser;
+
+import java.util.HashMap;
+
+@Disabled
+public class RSocketExecutorTest {
+
+    @Test
+    public void testRequestResponse() throws Exception {
+        @Language("HTTP Request")
+        String httpFileCode = """
+                ### RSocket Request
+                RSOCKET com.example.UserService.findById
+                Host: 127.0.0.1:42252
+                Content-Type: application/json
+                                 
+                1
+                """;
+        final HttpRequest httpRequest = HttpRequestParser.parse(httpFileCode, new HashMap<>()).get(0);
+        new RSocketExecutor().execute(httpRequest);
+    }
+
+    @Test
+    public void testRequestStream() throws Exception {
+        @Language("HTTP Request")
+        String httpFileCode = """
+                ### RSocket Request
+                STREAM com.example.UserService.findAll
+                Host: 127.0.0.1:42252
+                Content-Type: application/json
+                                 
+                1
+                """;
+        final HttpRequest httpRequest = HttpRequestParser.parse(httpFileCode, new HashMap<>()).get(0);
+        new RSocketExecutor().execute(httpRequest);
+    }
+}
