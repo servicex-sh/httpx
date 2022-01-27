4b58a61014b178c8d57669a0045b0710ec4f44ef
linux_china <libing.chen@gmail.com>
2022-01-26 18:47:17 -0800

    HttpMethod

---
+++ src/main/java/org/mvnsearch/http/model/HttpMethod.java
@@ -0,0 +1,53 @@
+package org.mvnsearch.http.model;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class HttpMethod {
+    public static final List<String> HTTP_METHODS = List.of("GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTION", "TRACE", "PATCH");
+    public static final List<String> RSOCKET_METHODS = List.of("RSOCKET", "RPC", "FNF", "STREAM", "METADATA_PUSH");
+    public static final List<String> GRPC_METHODS = List.of("GRPC");
+    private String name;
+
+    public HttpMethod() {
+    }
+
+    public HttpMethod(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static boolean isRequestLine(String line) {
+        final int offset = line.indexOf(' ');
+        String method;
+        if (offset < 0) {
+            method = line;
+        } else {
+            method = line.substring(0, offset);
+        }
+        return HTTP_METHODS.contains(method) || RSOCKET_METHODS.contains(method) || GRPC_METHODS.contains(method);
+    }
+
+    public static HttpMethod valueOf(String methodName) {
+        return new HttpMethod(methodName);
+    }
+
+    public boolean isHttpMethod() {
+        return HTTP_METHODS.contains(this.name);
+    }
+
+    public boolean isRSocketMethod() {
+        return RSOCKET_METHODS.contains(name);
+    }
+
+    public boolean isGrpcMethod() {
+        return GRPC_METHODS.contains(name);
+    }
+}
