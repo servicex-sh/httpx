a26c8b1b55f12951120f455f8d8ee822ad973ebf
linux_china <libing.chen@gmail.com>
2022-01-26 18:48:42 -0800

    HttpExecutor

---
+++ src/main/java/org/mvnsearch/http/protocol/HttpExecutor.java
@@ -0,0 +1,39 @@
+package org.mvnsearch.http.protocol;
+
+import org.mvnsearch.http.model.HttpHeader;
+import org.mvnsearch.http.model.HttpRequest;
+import reactor.netty.http.client.HttpClient;
+
+import java.net.URI;
+
+
+public class HttpExecutor {
+    public void execute(HttpRequest httpRequest) {
+        HttpClient client = HttpClient.create().headers(httpHeaders -> {
+            if (httpRequest.getHeaders() != null) {
+                for (HttpHeader header : httpRequest.getHeaders()) {
+                    httpHeaders.add(header.getName(), header.getValue());
+                }
+            }
+        });
+        final String httpMethod = httpRequest.getMethod().getName();
+        HttpClient.ResponseReceiver<?> responseReceiver = switch (httpMethod) {
+            case "POST" -> client.post().send(httpRequest.requestBody());
+            case "PUT" -> client.put().send(httpRequest.requestBody());
+            case "DELETE" -> client.delete();
+            case "HEAD" -> client.head();
+            default -> client.get();
+        };
+        final URI requestUri = httpRequest.getRequestTarget().getUri();
+        System.out.println(httpMethod + " " + requestUri);
+        System.out.println();
+        String body = responseReceiver
+                .uri(requestUri)
+                .response((response, byteBufFlux) -> {
+                    System.out.println("Status: " + response.status());
+                    response.responseHeaders().forEach(header -> System.out.println(header.getKey() + ": " + header.getValue()));
+                    return byteBufFlux.asString();
+                }).blockLast();
+        System.out.println(body);
+    }
+}
