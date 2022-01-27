8c26a8c88d7bcc27802eb2b7b5bbe4728f928c28
linux_china <libing.chen@gmail.com>
2022-01-27 11:14:09 -0800

    Add GRAPHQL_METHODS

--- src/main/java/org/mvnsearch/http/model/HttpMethod.java
+++ src/main/java/org/mvnsearch/http/model/HttpMethod.java
@@ -7,6 +7,7 @@ public class HttpMethod {
     public static final List<String> HTTP_METHODS = List.of("GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTION", "TRACE", "PATCH");
     public static final List<String> RSOCKET_METHODS = List.of("RSOCKET", "RPC", "FNF", "STREAM", "METADATA_PUSH");
     public static final List<String> GRPC_METHODS = List.of("GRPC");
+    public static final List<String> GRAPHQL_METHODS = List.of("GRAPHQL");
     private String name;
 
     public HttpMethod() {
@@ -32,7 +33,10 @@ public class HttpMethod {
         } else {
             method = line.substring(0, offset);
         }
-        return HTTP_METHODS.contains(method) || RSOCKET_METHODS.contains(method) || GRPC_METHODS.contains(method);
+        return HTTP_METHODS.contains(method)
+                || RSOCKET_METHODS.contains(method)
+                || GRPC_METHODS.contains(method)
+                || GRAPHQL_METHODS.contains(method);
     }
 
     public static HttpMethod valueOf(String methodName) {
