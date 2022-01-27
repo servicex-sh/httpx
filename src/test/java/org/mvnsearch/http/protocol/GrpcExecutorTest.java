bd851facff04c2ca473a804062560d92d491b666
linux_china <libing.chen@gmail.com>
2022-01-26 18:51:40 -0800

    GrpcExecutorTest

---
+++ src/test/java/org/mvnsearch/http/protocol/GrpcExecutorTest.java
@@ -0,0 +1,32 @@
+package org.mvnsearch.http.protocol;
+
+
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.http.model.HttpRequest;
+import org.mvnsearch.http.model.HttpRequestParser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Disabled
+public class GrpcExecutorTest {
+    @Test
+    public void testGrpcRequest() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        @Language("HTTP Request")
+        String httpFile = """
+                ### grpc call SayHello
+                //@name SayHello
+                GRPC localhost:50052/org.mvnsearch.service.Greeter/SayHello
+                                
+                {
+                   "name": "Jackie"
+                }
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        System.out.println(request.getRequestTarget().getUri());
+        new GrpcExecutor().execute(request);
+    }
+}
