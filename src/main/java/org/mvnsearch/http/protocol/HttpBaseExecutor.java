ef280d1998a0df55dc0b40df8fb83937778fbd95
linux_china <libing.chen@gmail.com>
2022-01-28 00:21:38 -0800

    Add cookies()

--- src/main/java/org/mvnsearch/http/protocol/HttpBaseExecutor.java
+++ src/main/java/org/mvnsearch/http/protocol/HttpBaseExecutor.java
@@ -2,7 +2,14 @@ package org.mvnsearch.http.protocol;
 
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import org.mvnsearch.http.model.HttpCookie;
 import reactor.netty.http.client.HttpClient;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class HttpBaseExecutor {
     protected HttpClient httpClient() {
@@ -13,5 +20,28 @@ public class HttpBaseExecutor {
 
             }
         });
+    }
+
+    protected List<HttpCookie> cookies(String domain) {
+        try {
+            final Path cookieFile = Path.of(".idea/httpRequests/http-client.cookies");
+            if (cookieFile.toFile().exists()) {
+                final List<String> lines = Files.readAllLines(cookieFile);
+                if (lines.size() > 1) {
+                    List<HttpCookie> cookies = new ArrayList<>();
+                    final long now = System.currentTimeMillis();
+                    for (String line : lines.subList(1, lines.size())) {
+                        final HttpCookie cookie = HttpCookie.valueOf(line);
+                        if (cookie.getDomain().equalsIgnoreCase(domain) && cookie.getExpired().getTime() > now) {
+                            cookies.add(cookie);
+                        }
+                    }
+                    return cookies;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
     }
 }
