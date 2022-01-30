139bab3750c7dfe9c9da371b6a2a146623bfb3cd
linux_china <libing.chen@gmail.com>
2022-01-29 16:08:58 -0800

    Add prettyJsonFormat()

--- src/main/java/org/mvnsearch/http/protocol/BaseExecutor.java
+++ src/main/java/org/mvnsearch/http/protocol/BaseExecutor.java
@@ -1,8 +1,10 @@
 package org.mvnsearch.http.protocol;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mvnsearch.http.model.HttpRequest;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BaseExecutor {
 
@@ -17,5 +19,22 @@ public interface BaseExecutor {
                 || contentType.contains("json")
                 || contentType.contains("xml")
                 || contentType.contains("yaml");
+    }
+
+    default String prettyJsonFormat(String jsonText) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            if (jsonText.startsWith("{")) {
+                final Map<?, ?> jsonObject = objectMapper.readValue(jsonText, Map.class);
+                return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+            } else if (jsonText.startsWith("[")) {
+                final List<?> jsonArray = objectMapper.readValue(jsonText, List.class);
+                return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonArray);
+            } else {
+                return jsonText;
+            }
+        } catch (Exception e) {
+            return jsonText;
+        }
     }
 }
