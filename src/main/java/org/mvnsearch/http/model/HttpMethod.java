580f467ad60d1d81dab3889084db76e9b7ca34f0
linux_china <libing.chen@gmail.com>
2022-01-31 14:37:47 -0800

    Add dubbo_methods

--- src/main/java/org/mvnsearch/http/model/HttpMethod.java
+++ src/main/java/org/mvnsearch/http/model/HttpMethod.java
@@ -8,6 +8,7 @@ public class HttpMethod {
     public static final List<String> RSOCKET_METHODS = List.of("RSOCKET", "RPC", "FNF", "STREAM", "METADATA_PUSH");
     public static final List<String> GRPC_METHODS = List.of("GRPC");
     public static final List<String> GRAPHQL_METHODS = List.of("GRAPHQL");
+    public static final List<String> DUBBO_METHODS = List.of("DUBBO");
     private String name;
 
     public HttpMethod() {
@@ -36,7 +37,8 @@ public class HttpMethod {
         return HTTP_METHODS.contains(method)
                 || RSOCKET_METHODS.contains(method)
                 || GRPC_METHODS.contains(method)
-                || GRAPHQL_METHODS.contains(method);
+                || GRAPHQL_METHODS.contains(method)
+                || DUBBO_METHODS.contains(method);
     }
 
     public static HttpMethod valueOf(String methodName) {
@@ -57,5 +59,9 @@ public class HttpMethod {
 
     public boolean isGraphQLMethod() {
         return GRAPHQL_METHODS.contains(name);
+    }
+
+    public boolean isDubboMethod() {
+        return DUBBO_METHODS.contains(name);
     }
 }
