461fac36d219ff60b95fb6e4ec14165e51306639
linux_china <libing.chen@gmail.com>
2022-02-01 16:11:48 -0800

    Use mail sender

--- src/main/java/org/mvnsearch/http/protocol/MailExecutor.java
+++ src/main/java/org/mvnsearch/http/protocol/MailExecutor.java
@@ -2,16 +2,15 @@ package org.mvnsearch.http.protocol;
 
 import org.mvnsearch.http.model.HttpRequest;
 import org.mvnsearch.http.model.SmtpRequest;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
-import javax.mail.*;
+import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.net.ssl.SSLSocketFactory;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 
 public class MailExecutor extends HttpBaseExecutor {
@@ -21,14 +20,20 @@ public class MailExecutor extends HttpBaseExecutor {
             System.err.print("Format is not correct, and failed to send email!");
             return Collections.emptyList();
         }
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         Properties prop = new Properties();
+        prop.put("mail.transport.protocol", "smtp");
         prop.put("mail.smtp.host", smtpRequest.getHost());
         prop.put("mail.smtp.port", smtpRequest.getPort());
         prop.put("mail.smtp.socketFactory.port", smtpRequest.getPort());
-        if (smtpRequest.isSsl()) {
-            prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        if (smtpRequest.getSchema() != null) {
+            if (Objects.equals(smtpRequest.getSchema(), "ssl")) {
+                SSLSocketFactory.getDefault();
+                prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            } else if (Objects.equals(smtpRequest.getSchema(), "tls")) {
+                prop.put("mail.smtp.starttls.enable", "true");
+            }
         }
-        Authenticator authenticator = null;
         final String authorization = smtpRequest.getAuthorization();
         if (authorization != null) {
             prop.put("mail.smtp.auth", "true");
@@ -38,19 +43,16 @@ public class MailExecutor extends HttpBaseExecutor {
                     pair = new String(Base64.getDecoder().decode(pair), StandardCharsets.UTF_8);
                 }
                 final String[] parts = pair.split(":");
-                authenticator = new Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(parts[0], parts[1]);
-                    }
-                };
+                mailSender.setUsername(parts[0]);
+                mailSender.setPassword(parts[1]);
             }
         }
         System.out.println("MAIL " + smtpRequest.getUrl());
         System.out.println();
         try {
-            Session session = Session.getInstance(prop, authenticator);
-            MimeMessage message = new MimeMessage(session);
-            MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+            mailSender.setJavaMailProperties(prop);
+            final MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
             helper.setFrom(new InternetAddress(smtpRequest.getFrom()));
             helper.setTo(InternetAddress.parse(smtpRequest.getTo()));
             if (smtpRequest.getCc() != null) {
@@ -64,11 +66,12 @@ public class MailExecutor extends HttpBaseExecutor {
             if (smtpRequest.getContentType().equals("text/html")) {
                 helper.setText(body, true);
             } else {
-                message.setText(body);
+                helper.setText(body);
             }
-            Transport.send(message);
+            mailSender.send(mimeMessage);
             System.out.print("Succeeded to send email!");
         } catch (MessagingException e) {
+            e.printStackTrace();
             System.err.print("Failed to send email: " + e.getMessage());
         }
         return Collections.emptyList();
