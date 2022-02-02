3c50edc3a9dc281b20843ac67676cfcabc16e7bc
linux_china <libing.chen@gmail.com>
2022-02-02 11:00:53 -0800

    Add NativeHint

--- src/main/java/org/mvnsearch/http/HttpxApplication.java
+++ src/main/java/org/mvnsearch/http/HttpxApplication.java
@@ -2,8 +2,11 @@ package org.mvnsearch.http;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ResourceHint;
 
 @SpringBootApplication
+@NativeHint(resources = {@ResourceHint(patterns = {"httpx/ErrorMessages.properties",})})
 public class HttpxApplication {
 
     public static void main(String[] args) {
