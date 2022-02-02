a157196d2289b1478f89b50f69d0decd38905a0c
linux_china <libing.chen@gmail.com>
2022-02-01 17:16:35 -0800

    Add MailSSLSocketFactory & SSLSocketFactory

--- src/main/java/org/mvnsearch/http/aot/ThirdLibrariesHints.java
+++ src/main/java/org/mvnsearch/http/aot/ThirdLibrariesHints.java
@@ -1,10 +1,12 @@
 package org.mvnsearch.http.aot;
 
+import com.sun.mail.util.MailSSLSocketFactory;
 import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.BeanFactoryNativeConfigurationProcessor;
 import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.NativeConfigurationRegistry;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.nativex.hint.TypeAccess;
 
+import javax.net.ssl.SSLSocketFactory;
 import java.sql.Date;
 import java.sql.Time;
 import java.sql.Timestamp;
@@ -16,5 +18,9 @@ public class ThirdLibrariesHints implements BeanFactoryNativeConfigurationProces
         registry.reflection().forType(Date.class).withAccess(TypeAccess.DECLARED_CONSTRUCTORS).build();
         registry.reflection().forType(Time.class).withAccess(TypeAccess.DECLARED_CONSTRUCTORS).build();
         registry.reflection().forType(Timestamp.class).withAccess(TypeAccess.DECLARED_CONSTRUCTORS).build();
+        registry.reflection().forType(MailSSLSocketFactory.class).withAccess(TypeAccess.DECLARED_CONSTRUCTORS)
+                .withAccess(TypeAccess.DECLARED_METHODS).withAccess(TypeAccess.DECLARED_FIELDS).build();
+        registry.reflection().forType(SSLSocketFactory.class).withAccess(TypeAccess.DECLARED_CONSTRUCTORS)
+                .withAccess(TypeAccess.DECLARED_METHODS).withAccess(TypeAccess.DECLARED_FIELDS).build();
     }
 }
