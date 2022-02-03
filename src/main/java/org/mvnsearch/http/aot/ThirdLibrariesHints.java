885acf638b9b36e12bad026c05be398c217cd29d
linux_china <libing.chen@gmail.com>
2022-02-02 17:23:05 -0800

    Add kafka

--- src/main/java/org/mvnsearch/http/aot/ThirdLibrariesHints.java
+++ src/main/java/org/mvnsearch/http/aot/ThirdLibrariesHints.java
@@ -1,8 +1,20 @@
 package org.mvnsearch.http.aot;
 
 import com.sun.mail.util.MailSSLSocketFactory;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.RangeAssignor;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.internals.DefaultPartitioner;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.BeanFactoryNativeConfigurationProcessor;
 import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.NativeConfigurationRegistry;
+import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.NativeResourcesEntry;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.nativex.hint.TypeAccess;
 
@@ -15,12 +27,23 @@ import java.sql.Timestamp;
 public class ThirdLibrariesHints implements BeanFactoryNativeConfigurationProcessor {
     @Override
     public void process(ConfigurableListableBeanFactory beanFactory, NativeConfigurationRegistry registry) {
+        // hessian
         registry.reflection().forType(Date.class).withAccess(TypeAccess.DECLARED_CONSTRUCTORS).build();
         registry.reflection().forType(Time.class).withAccess(TypeAccess.DECLARED_CONSTRUCTORS).build();
         registry.reflection().forType(Timestamp.class).withAccess(TypeAccess.DECLARED_CONSTRUCTORS).build();
+        // javamail
         registry.reflection().forType(MailSSLSocketFactory.class).withAccess(TypeAccess.DECLARED_CONSTRUCTORS)
                 .withAccess(TypeAccess.DECLARED_METHODS).withAccess(TypeAccess.DECLARED_FIELDS).build();
         registry.reflection().forType(SSLSocketFactory.class).withAccess(TypeAccess.DECLARED_CONSTRUCTORS)
                 .withAccess(TypeAccess.DECLARED_METHODS).withAccess(TypeAccess.DECLARED_FIELDS).build();
+        // kafka
+        final Class<?>[] kafkaClassArray = {ConsumerConfig.class, KafkaConsumer.class, RangeAssignor.class,
+                KafkaProducer.class, ProducerConfig.class, ProducerRecord.class, DefaultPartitioner.class,
+                LongDeserializer.class, LongSerializer.class, StringDeserializer.class, StringSerializer.class};
+        for (Class<?> clazz : kafkaClassArray) {
+            registry.reflection().forType(clazz).withAccess(TypeAccess.DECLARED_CONSTRUCTORS)
+                    .withAccess(TypeAccess.DECLARED_METHODS).withAccess(TypeAccess.DECLARED_FIELDS).build();
+        }
+        registry.resources().add(NativeResourcesEntry.of("kafka/kafka-version.properties"));
     }
 }
