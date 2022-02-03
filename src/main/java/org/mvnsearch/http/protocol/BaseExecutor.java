776c4e9387977fd4b691a7b34dce69ceb98f68ea
linux_china <libing.chen@gmail.com>
2022-02-02 17:11:28 -0800

    Add queryToMap

--- src/main/java/org/mvnsearch/http/protocol/BaseExecutor.java
+++ src/main/java/org/mvnsearch/http/protocol/BaseExecutor.java
@@ -3,6 +3,11 @@ package org.mvnsearch.http.protocol;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mvnsearch.http.model.HttpRequest;
 
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -36,5 +41,24 @@ public interface BaseExecutor {
         } catch (Exception e) {
             return jsonText;
         }
+    }
+
+    default Map<String, String> queryToMap(URI url) {
+        String queryPart = url.getQuery();
+        if (queryPart == null || queryPart.isEmpty()) {
+            return Collections.emptyMap();
+        }
+        Map<String, String> queryParams = new HashMap<>();
+        try {
+            String[] pairs = queryPart.split("&");
+            for (String pair : pairs) {
+                String[] keyValuePair = pair.split("=");
+                queryParams.put(URLDecoder.decode(keyValuePair[0], StandardCharsets.UTF_8.name()),
+                        URLDecoder.decode(keyValuePair[1], StandardCharsets.UTF_8.name()));
+            }
+        } catch (Exception ignore) {
+
+        }
+        return queryParams;
     }
 }
