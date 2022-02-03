a413673e8aafb619926611d3189a71f13433e522
linux_china <libing.chen@gmail.com>
2022-02-02 17:33:21 -0800

    MessagePubExecutorTest

---
+++ src/test/java/org/mvnsearch/http/protocol/MessagePubExecutorTest.java
@@ -0,0 +1,33 @@
+package org.mvnsearch.http.protocol;
+
+
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.http.model.HttpRequest;
+import org.mvnsearch.http.model.HttpRequestParser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Disabled
+public class MessagePubExecutorTest {
+    @Test
+    public void testSendMessage() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        @Language("HTTP Request")
+        String httpFile = """
+                ### send kafka message
+                //@name kafka
+                PUB topic:kafka://localhost:9092/topic-1
+                Content-Type: application/json
+                               
+                {
+                  "name": "Jackie"
+                }
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        request.cleanBody();
+        new MessagePublishExecutor().execute(request);
+    }
+}
