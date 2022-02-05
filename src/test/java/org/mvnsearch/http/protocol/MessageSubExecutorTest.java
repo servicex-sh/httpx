91bdac54021748f7497a795c57c2dfd6dd094a4b
linux_china <libing.chen@gmail.com>
2022-02-04 22:31:32 -0800

    Add testSubscribeRocketmq()

--- src/test/java/org/mvnsearch/http/protocol/MessageSubExecutorTest.java
+++ src/test/java/org/mvnsearch/http/protocol/MessageSubExecutorTest.java
@@ -54,4 +54,18 @@ public class MessageSubExecutorTest {
         request.cleanBody();
         new MessageSubscribeExecutor().execute(request);
     }
+
+    @Test
+    public void testSubscribeRocketmq() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        @Language("HTTP Request")
+        String httpFile = """
+                ### subscribe rabbitmq
+                SUB testTopic
+                Host: rocketmq://localhost:9876
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        request.cleanBody();
+        new MessageSubscribeExecutor().execute(request);
+    }
 }
