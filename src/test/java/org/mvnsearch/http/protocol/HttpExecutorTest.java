91b149ebbfddd3566083e33cb948afd70abaeefe
linux_china <libing.chen@gmail.com>
2022-02-14 12:26:59 -0800

    Add testHttpRequestGetIp

--- src/test/java/org/mvnsearch/http/protocol/HttpExecutorTest.java
+++ src/test/java/org/mvnsearch/http/protocol/HttpExecutorTest.java
@@ -6,13 +6,26 @@ import org.junit.jupiter.api.Test;
 import org.mvnsearch.http.model.HttpRequest;
 import org.mvnsearch.http.model.HttpRequestParser;
 
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpExecutorTest {
+
+    @Test
+    public void testHttpRequestGetIp() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        @Language("HTTP Request")
+        String httpFile = """
+                ### hello ip
+                GET https://httpbin.org/ip
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        request.cleanBody();
+        new HttpExecutor().execute(request);
+    }
+
     @Test
     public void testHttpRequest() throws Exception {
         Map<String, Object> context = new HashMap<>();
