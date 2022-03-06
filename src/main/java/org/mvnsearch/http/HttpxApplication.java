71f2ab127c144692c9ed264f04690806218fd321
linux_china <libing.chen@gmail.com>
2022-03-06 15:17:56 -0800

    Exclude rsocket autoconfiguration because spring-messaging introduced

--- src/main/java/org/mvnsearch/http/HttpxApplication.java
+++ src/main/java/org/mvnsearch/http/HttpxApplication.java
@@ -2,10 +2,12 @@ package org.mvnsearch.http;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.rsocket.RSocketRequesterAutoConfiguration;
+import org.springframework.boot.autoconfigure.rsocket.RSocketStrategiesAutoConfiguration;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.ResourceHint;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RSocketStrategiesAutoConfiguration.class, RSocketRequesterAutoConfiguration.class})
 @NativeHint(resources = {@ResourceHint(patterns = {"httpx/ErrorMessages.properties",})})
 public class HttpxApplication {
 
