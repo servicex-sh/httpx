191ade71ea18ee57a07654b2638c810ac2bd9d77
linux_china <libing.chen@gmail.com>
2022-03-06 15:55:52 -0800

    Add RSocketStrategies bean

--- src/main/java/org/mvnsearch/http/HttpxApplication.java
+++ src/main/java/org/mvnsearch/http/HttpxApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.rsocket.RSocketRequesterAutoConfiguration;
 import org.springframework.boot.autoconfigure.rsocket.RSocketStrategiesAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.rsocket.RSocketStrategies;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.ResourceHint;
 
@@ -13,6 +15,11 @@ public class HttpxApplication {
 
     public static void main(String[] args) {
         System.exit(SpringApplication.exit(SpringApplication.run(HttpxApplication.class, args)));
+    }
+
+    @Bean
+    public RSocketStrategies rSocketStrategies() {
+        return RSocketStrategies.builder().build();
     }
 
 }
