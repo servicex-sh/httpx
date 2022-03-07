625d99884fdb414da3f82d3781e037f0cf85877e
linux_china <libing.chen@gmail.com>
2022-03-06 16:15:12 -0800

    Exclude RSocket Configurations

--- src/main/java/org/mvnsearch/http/HttpxApplication.java
+++ src/main/java/org/mvnsearch/http/HttpxApplication.java
@@ -2,24 +2,24 @@ package org.mvnsearch.http;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.rsocket.RSocketMessagingAutoConfiguration;
 import org.springframework.boot.autoconfigure.rsocket.RSocketRequesterAutoConfiguration;
+import org.springframework.boot.autoconfigure.rsocket.RSocketServerAutoConfiguration;
 import org.springframework.boot.autoconfigure.rsocket.RSocketStrategiesAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.rsocket.RSocketStrategies;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.ResourceHint;
 
-@SpringBootApplication(exclude = {RSocketStrategiesAutoConfiguration.class, RSocketRequesterAutoConfiguration.class})
+@SpringBootApplication(
+        exclude = {
+                RSocketStrategiesAutoConfiguration.class,
+                RSocketMessagingAutoConfiguration.class,
+                RSocketRequesterAutoConfiguration.class,
+                RSocketServerAutoConfiguration.class
+        })
 @NativeHint(resources = {@ResourceHint(patterns = {"httpx/ErrorMessages.properties",})})
 public class HttpxApplication {
 
     public static void main(String[] args) {
         System.exit(SpringApplication.exit(SpringApplication.run(HttpxApplication.class, args)));
     }
-
-    @Bean
-    public RSocketStrategies rSocketStrategies() {
-        return RSocketStrategies.builder().build();
-    }
-
 }
