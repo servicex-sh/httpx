da6418319079a388a51dbb1f023110abfc337750
linux_china <libing.chen@gmail.com>
2022-03-09 15:11:20 -0800

    Add AWS_METHODS

--- src/main/java/org/mvnsearch/http/model/HttpMethod.java
+++ src/main/java/org/mvnsearch/http/model/HttpMethod.java
@@ -14,6 +14,7 @@ public class HttpMethod {
     public static final List<String> ZEROMQ_METHODS = List.of("ZEROREQ");
     public static final List<String> MAIL_METHODS = List.of("MAIL");
     public static final List<String> ALIYUN_METHODS = List.of("ALIYUN", "ALICLOUD");
+    public static final List<String> AWS_METHODS = List.of("AWS");
     public static final List<String> PUB_METHODS = List.of("PUB");
     public static final List<String> SUB_METHODS = List.of("SUB");
     private String name;
@@ -48,6 +49,7 @@ public class HttpMethod {
                 || DUBBO_METHODS.contains(method)
                 || THRIFT_METHODS.contains(method)
                 || ALIYUN_METHODS.contains(method)
+                || AWS_METHODS.contains(method)
                 || ZEROMQ_METHODS.contains(method)
                 || SOFA_METHODS.contains(method)
                 || MAIL_METHODS.contains(method)
@@ -97,6 +99,10 @@ public class HttpMethod {
 
     public boolean isAliyunMethod() {
         return ALIYUN_METHODS.contains(name);
+    }
+
+    public boolean isAwsMethod() {
+        return AWS_METHODS.contains(name);
     }
 
     public boolean isPubMethod() {
