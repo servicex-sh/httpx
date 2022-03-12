9d8bdba08cd78e93e36926bd5951efddbeabdf72
linux_china <libing.chen@gmail.com>
2022-03-11 16:24:07 -0800

    NodejsTest

---
+++ src/test/java/org/mvnsearch/http/vendor/NodejsTest.java
@@ -0,0 +1,27 @@
+package org.mvnsearch.http.vendor;
+
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NodejsTest {
+    @Test
+    public void testExecuteJsBlock() throws Exception {
+        @Language("JavaScript")
+        String jsCode = """
+                client.test("Request executed successfully", function () {
+                    client.log(response.status)
+                    client.log(response.contentType)
+                    client.log(response.body)
+                });
+                """;
+        int statusCode = 200;
+        String contentType = "text/plain";
+        String body = "hello world";
+        Map<String, String> headers = new HashMap<>();
+        String result = Nodejs.executeHttpClientCode(jsCode, statusCode, headers, contentType, body);
+        System.out.println(result);
+    }
+}
