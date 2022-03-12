573128bc372338b39b2ccb090c255d7b69cf6f6a
linux_china <libing.chen@gmail.com>
2022-03-11 16:53:31 -0800

    Add jsCode support

--- src/main/java/org/mvnsearch/http/protocol/AliyunExecutor.java
+++ src/main/java/org/mvnsearch/http/protocol/AliyunExecutor.java
@@ -15,6 +15,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class AliyunExecutor implements BaseExecutor {
     private static final HttpxErrorCodeLogger log = HttpxErrorCodeLoggerFactory.getLogger(AliyunExecutor.class);
@@ -67,8 +68,11 @@ public class AliyunExecutor implements BaseExecutor {
             }
             request.putQueryParameter("Format", format);
             CommonResponse response = client.getCommonResponse(request);
+            final Map<String, String> sysHeaders = response.getHttpResponse().getSysHeaders();
+            String contentType = Objects.equals(format, "JSON") ? "application/json" : "application/xml";
             String text = response.getData();
             System.out.print(prettyJsonFormatWithJsonPath(text, httpRequest.getHeader("X-JSON-PATH")));
+            runJsTest(httpRequest, response.getHttpStatus(), sysHeaders, contentType, text);
             return List.of(text.getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
             log.error("HTX-101-500", e);
