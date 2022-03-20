a1462492b3e7c46a3d1158b0fa744330f210dcdb
linux_china <libing.chen@gmail.com>
2022-03-19 18:17:17 -0700

    HttpxExtensionResponse

---
+++ src/main/java/org/mvnsearch/http/model/extension/HttpxExtensionResponse.java
@@ -0,0 +1,42 @@
+package org.mvnsearch.http.model.extension;
+
+import java.util.Map;
+
+public class HttpxExtensionResponse {
+    private int status;
+    private String message;
+    private Map<String, String> headers;
+    private byte[] body;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
+}
