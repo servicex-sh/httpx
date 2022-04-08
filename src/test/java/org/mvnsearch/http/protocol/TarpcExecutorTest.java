e22f218b33aa9d9473f6af9bc93150bfb7c23f56
linux_china <libing.chen@gmail.com>
2022-04-08 15:23:49 -0700

    TarpcExecutorTest

---
+++ src/test/java/org/mvnsearch/http/protocol/TarpcExecutorTest.java
@@ -0,0 +1,32 @@
+package org.mvnsearch.http.protocol;
+
+
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.http.model.HttpRequest;
+import org.mvnsearch.http.model.HttpRequestParser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Disabled
+public class TarpcExecutorTest {
+    @Test
+    public void testReqResp() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        @Language("HTTP Request")
+        String httpFile = """
+                ### thrift request
+                TARPC 127.0.0.1:4500/hello
+                Content-Type: application/json
+                     
+                {
+                  "name": "jackie"
+                }    
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        request.cleanBody();
+        new TarpcExecutor().execute(request);
+    }
+}
