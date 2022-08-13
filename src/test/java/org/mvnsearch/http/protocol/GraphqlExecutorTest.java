1ea1ce1e85ead4d07c2407ab6b61e8835490eb1b
linux_china <libing.chen@gmail.com>
2022-08-12 17:54:22 -0700

    test: add testGraphQLWithVariables

--- src/test/java/org/mvnsearch/http/protocol/GraphqlExecutorTest.java
+++ src/test/java/org/mvnsearch/http/protocol/GraphqlExecutorTest.java
@@ -48,6 +48,35 @@ public class GraphqlExecutorTest {
     }
 
     @Test
+    public void testGraphQLWithVariables() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        String httpFile = """
+                ### GraphQL query with variables
+                GRAPHQL https://httpbin.org/post
+                                
+                query demo($bookId: ID){
+                    bookById(id: $bookId) {
+                        id
+                        name
+                        pageCount
+                        author {
+                            firstName
+                            lastName
+                        }
+                    }
+                }
+                                
+                {
+                  "bookId": "book-1"
+                }
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        request.cleanBody();
+        final List<byte[]> bytesList = new GraphqlExecutor().execute(request);
+        System.out.println("======size: " + bytesList.size());
+    }
+
+    @Test
     public void testWebSocketSubscribe() throws Exception {
         Map<String, Object> context = new HashMap<>();
         String httpFile = """
