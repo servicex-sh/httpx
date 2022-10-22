6f3b3da824341510f8de26550d19bf80a8d6e6b4
linux_china <libing.chen@gmail.com>
2022-10-22 16:52:28 +0800

    test: add testHttpRequestPreCode() to test pre script

--- src/test/java/org/mvnsearch/http/protocol/HttpExecutorTest.java
+++ src/test/java/org/mvnsearch/http/protocol/HttpExecutorTest.java
@@ -19,9 +19,56 @@ public class HttpExecutorTest {
         @Language("HTTP Request")
         String httpFile = """
                 ### hello ip
-                GET https://httpbin.org/get
-                  ?id=1
-                  &name=xxx
+                GET https://httpbin.org/get?
+                  id=1&
+                  name=xxx
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        request.cleanBody();
+        new HttpExecutor().execute(request);
+    }
+
+    @Test
+    public void testHttpRequestPreCode() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        @Language("HTTP Request")
+        String httpFile = """
+                ### hello pre script
+                < {%
+                  request.variables.set("name","linux_china");
+                  request.variables.set("name2","name2");
+                %}
+                #@name demo
+                GET https://httpbin.org/get?id=1&name={{name}}
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        request.cleanBody();
+        new HttpExecutor().execute(request);
+    }
+
+    @Test
+    public void testHttpStatusCode() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        @Language("HTTP Request")
+        String httpFile = """
+                ### hello status
+                POST https://httpbin.org/status/401
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        request.cleanBody();
+        new HttpExecutor().execute(request);
+    }
+
+    @Test
+    public void testDownloadFile() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        context.put("logo", "https://httpbin.org/ip");
+        @Language("HTTP Request")
+        String httpFile = """
+                ### hello
+                GET {{logo}}
+                                
+                >>! temp/demo.txt
                 """;
         HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
         request.cleanBody();
@@ -49,10 +96,26 @@ public class HttpExecutorTest {
                 ### hello ip
                 POST https://httpbin.org/post
                 Content-Type: application/x-www-form-urlencoded
-                
+                                
                 foo1=bar
                 &foo2=bar
                 &foo3=bar
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        request.cleanBody();
+        new HttpExecutor().execute(request);
+    }
+
+    @Test
+    public void testHttpPostWithRandom() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        @Language("HTTP Request")
+        String httpFile = """
+                ### hello post
+                POST https://httpbin.org/post
+                Content-Type: application/x-www-form-urlencoded
+                                
+                uuid={{$random.hexadecimal}}
                 """;
         HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
         request.cleanBody();
