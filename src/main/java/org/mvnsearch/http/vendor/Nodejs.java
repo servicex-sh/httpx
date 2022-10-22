01a222e1632a1a428d52fa0d9f8a4f693d698d6d
linux_china <libing.chen@gmail.com>
2022-10-22 16:42:25 +0800

    feat: add executePreScriptCode() to execute pre script code

--- src/main/java/org/mvnsearch/http/vendor/Nodejs.java
+++ src/main/java/org/mvnsearch/http/vendor/Nodejs.java
@@ -13,6 +13,7 @@ import java.util.Map;
 public class Nodejs {
     private static final HttpxErrorCodeLogger log = HttpxErrorCodeLoggerFactory.getLogger(Nodejs.class);
     private static String httpClientExecuteJS = null;
+    private static String httpClientPreScriptJS = null;
 
     public static String executeHttpClientCode(String jsBlockCode, int statusCode, Map<String, String> headers, String contentType, String body) {
         try {
@@ -51,6 +52,37 @@ public class Nodejs {
             }
         } catch (Exception e) {
             log.error("HTX-001-503", jsBlockCode, e);
+            return "";
+        }
+    }
+
+    public static String executePreScriptCode(String preScriptCode) {
+        try {
+            if (httpClientPreScriptJS == null) {
+                //noinspection ConstantConditions
+                httpClientPreScriptJS = IOUtils.toString(Nodejs.class.getResourceAsStream("/http-client-pre-stub.js"), StandardCharsets.UTF_8);
+            }
+            String jsCode = httpClientPreScriptJS + preScriptCode;
+            ProcessBuilder pb = new ProcessBuilder("node");
+            Process p = pb.start();
+            p.getOutputStream().write(jsCode.getBytes(StandardCharsets.UTF_8));
+            p.getOutputStream().close();
+            final int exitCode = p.waitFor();
+            if (exitCode == 0) {
+                return IOUtils.toString(p.getInputStream(), StandardCharsets.UTF_8);
+            } else {
+                final String error = IOUtils.toString(p.getErrorStream(), StandardCharsets.UTF_8);
+                if (error.contains("No such file")) {
+                    if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                        System.err.println("Please use 'brew install node' to install Node.js first!");
+                    } else {
+                        System.err.println("Please install node.js first! Please click https://nodejs.org/en/download/");
+                    }
+                }
+                return "";
+            }
+        } catch (Exception e) {
+            log.error("HTX-001-503", preScriptCode, e);
             return "";
         }
     }
