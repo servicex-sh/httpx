d97af82473cf902c6eb20f6fcb4c5db9d1a2adef
Libing Chen <libing.chen@gmail.com>
2023-07-03 19:42:23 +0800

    test: add testGrpcTlsTest()

--- src/test/java/org/mvnsearch/http/protocol/GrpcExecutorTest.java
+++ src/test/java/org/mvnsearch/http/protocol/GrpcExecutorTest.java
@@ -19,10 +19,29 @@ public class GrpcExecutorTest {
         String httpFile = """
                 ### grpc call SayHello
                 //@name SayHello
-                GRPC localhost:50052/org.mvnsearch.service.Greeter/SayHello
+                GRPC http://grpcb.in:9000/hello.HelloService/SayHello
                                 
                 {
-                   "name": "Jackie"
+                  "greeting": "Jackie"
+                }
+                """;
+        HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
+        request.cleanBody();
+        System.out.println(request.getRequestTarget().getUri());
+        new GrpcExecutor().execute(request);
+    }
+
+    @Test
+    public void testGrpcTlsTest() throws Exception {
+        Map<String, Object> context = new HashMap<>();
+        @Language("HTTP Request")
+        String httpFile = """
+                ### grpc call SayHello
+                //@name SayHello
+                GRPC grpcs://grpcb.in:9001/hello.HelloService/SayHello
+                                
+                {
+                  "greeting": "Jackie"
                 }
                 """;
         HttpRequest request = HttpRequestParser.parse(httpFile, context).get(0);
